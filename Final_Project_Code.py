1e6bc6ed05e47639a01dab6b68a1e7882b7a5652
ecedano1 <54921157+ecedano1@users.noreply.github.com>
2019-12-09 00:50:10 -0500

    up

--- Final_Project_Code.py
+++ Final_Project_Code.py
@@ -46,7 +46,7 @@ def take_photo():
     filename = "{0:%Y}-{0:%m}-{0:%H}-{0:%M}-{0:%S}.png".format(now)
     camera.start_preview(alpha=190)
     sleep(1)
-    camera.capture("/home/pi/Desktop/TweetingBabbage/{0}".format(filename))
+    camera.capture("/home/pi/Desktop/TweetingBabbage/Photos{0}".format(filename))
     camera.stop_preview()
     # Code to get the timstamp
 	# Then take a photo and save it 
@@ -59,9 +59,11 @@ def send_tweet():
 	##code to send tweet
 
 #final action command that prompts the teddy bear to take the photo and then send the tweet
+
+@app.route("/Final_Project_Code", methods= ["POST"])
 def main():
     take_photo()
-    # send_tweet()
+    send_tweet()
 
 
 ## Code to trigger the function when the button is pressed
